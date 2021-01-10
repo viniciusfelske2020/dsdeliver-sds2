1fdc7d91650ee309c2bfd0d46a3859f481a0f604
unknown <viniciusfelske@gmail.com>
2021-01-10 15:59:51 -0300

    Add Homepage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 //import Hello from "./Hello";
 //import Counter from "./Counter";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 }
 
