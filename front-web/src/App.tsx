a959a61842c18054326ce1ca9be77197138fda85
unknown <viniciusfelske@gmail.com>
2021-01-10 15:09:08 -0300

    Add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 //import Hello from "./Hello";
 //import Counter from "./Counter";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div >
-      <h2>HELOO</h2>
+      <Navbar/>
     </div>
   );
 }
