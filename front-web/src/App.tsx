9624a19064433f6e59eab8e5e0122d9062f0af22
unknown <viniciusfelske@gmail.com>
2021-01-10 14:31:25 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+//import Hello from "./Hello";
+//import Counter from "./Counter";
+
+function App() {
+  return (
+    <div >
+      <h2>HELOO</h2>
+    </div>
+  );
+}
+
+export default App;
