e7fd0c72d19c8a5e8eeaa5738fad977be83b9ee0
unknown <viniciusfelske@gmail.com>
2021-01-10 17:16:00 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 //import Hello from "./Hello";
 //import Counter from "./Counter";
 import Navbar from "./Navbar";
-import Home from "./Home";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    
+      
+      <Routes />
+    
   );
 }
 
